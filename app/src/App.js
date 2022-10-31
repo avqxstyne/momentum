5d352796f2c515fc2dc8289f3ecc3911e1597712
August <augustwiesner16@gmail.com>
2022-10-31 17:32:39 -0500

    i dont really knwo

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+   <div className="body">
+      <nav className="main-nav">
+        <h1 className="main-nav-heading">Counters</h1>
+      </nav>
+      <div className="sidebar">
+        bruh
+      </div>
+   </div>
+  );
+}
+
+export default App;
