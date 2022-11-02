4e846b66c12aaf007fa99ffb0cf6b6510d8c53ed
August <augustwiesner16@gmail.com>
2022-11-02 18:45:29 -0500

    im doing it for the color

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,15 +3,22 @@ import * as React from "react";
 
 function App() {
   return (<>
-   <div className="body">
+    <div className="body">
       <nav className="main-nav">
         <h1 className="main-nav-heading">Counters</h1>
       </nav>
-      <div className='counter'>ooo</div>
-   </div> 
-   <div className="sidebar">
-   bruh
- </div></>
+      <div className='counter-container'>
+        <div className='counter'>A</div>        
+        <div className='counter'>B</div>
+        <div className='counter'>C</div>
+      </div>
+    </div> 
+    <div className="sidebar">
+    bruh
+    </div>
+    <div className="sidebar">
+    bruh
+    </div></>
   );
 }
 
